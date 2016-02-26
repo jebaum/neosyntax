c17ad79ca42e387ca234841ecca985e4d646121a
James Baumgarten <jebaum@ucla.edu>
2016-02-26 15:39:40 -0800

    generalize printtokens utility script

--- printtokens.py
+++ printtokens.py
@@ -2,14 +2,22 @@
 import neovim
 import pygments
 import pygments.lexers
+import sys
 
-nvim = neovim.attach('socket', path='/tmp/nvim')
-buf = nvim.buffers[0]
 
-mylexer = pygments.lexers.PythonLexer()
-fulbuf = [line.decode("utf-8") for line in buf]
+if len(sys.argv) != 2:
+    print('please provide exactly one filename')
+    sys.exit()
 
-for linenum, line in enumerate(fulbuf, start=0):
+filename = str(sys.argv[1])
+lines    = [line for line in open(filename)]
+numlines = len(lines)
+maxlen   = len(str(numlines))
+mylexer  = pygments.lexers.guess_lexer_for_filename(filename, lines)
+
+for linenum, line in enumerate(lines, start=1):
     for (index, tokentype, value) in mylexer.get_tokens_unprocessed(line):
-        cols = len(value)
-        print(tokentype, value, cols)
+        if value.rstrip() != '':
+            print(("Line {:" + str(maxlen) + "d}: '{}' matches '{}'").format(linenum, tokentype, value.rstrip()))
+
+print("Lexer used: " + mylexer.name)
