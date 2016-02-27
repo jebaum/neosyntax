ac6219a2bf4df0aaa34077cbf7984b423de33bce
James Baumgarten <jebaum@ucla.edu>
2016-02-27 03:59:09 -0800

    implement swapping src_id to avoid some flicker

--- rplugin/python3/neosyntax.py
+++ rplugin/python3/neosyntax.py
@@ -8,14 +8,20 @@ import pygments.token
 
 class Neosyntax(object):
     def __init__(self, nvim):
-        self.nvim = nvim
+        self.nvim    = nvim
+        #   swap src_ids. from brefdl: allocate two ids, and swap, adding before clearing, so things that don't change won't appear to flicker
+        self.srcset  = True
 
-    @neovim.autocmd('TextChanged', pattern='*.py', eval='expand("<afile>")', sync=False)
-    def autocmd_handler(self, filename):
+    @neovim.autocmd('BufEnter', pattern='nvim.py', eval='expand("<afile>")', sync=False)
+    def autocmd_handler1(self, filename):
         self.highlight_buffer(None)
 
-    @neovim.autocmd('TextChangedI', pattern='*.py', eval='expand("<afile>")', sync=False)
-    def autocmd_handler(self, filename):
+    @neovim.autocmd('TextChanged', pattern='nvim.py', eval='expand("<afile>")', sync=False)
+    def autocmd_handler2(self, filename):
+        self.highlight_buffer(None)
+
+    @neovim.autocmd('TextChangedI', pattern='nvim.py', eval='expand("<afile>")', sync=False)
+    def autocmd_handler3(self, filename):
         # TODO I was hoping that performance with syntax highlighting being done by this autocmd
         # would be comparable to plain old :syntax off and without this plugin
         # I think it is better, although I'll have to find a way to test that empirically
@@ -28,14 +34,30 @@ class Neosyntax(object):
         # This is less than ideal for lots of situations, but is better than nothing
         self.highlight_buffer(None)
 
+    @neovim.function('UnHighlightBuffer', sync=False)
+    def unhighlight_buffer(self, args):
+        buf     = self.nvim.buffers[0] # TODO can't hardcode this
+        buf.clear_highlight(src_id=0, line_start=0, line_end=1000, async=True)
+        buf.clear_highlight(src_id=1, line_start=0, line_end=1000, async=True)
+
+
     @neovim.function('HighlightBuffer', sync=False)
     def highlight_buffer(self, args):
+        # XXX some ideas to help with flickering:
+        #   precompute entire buffer before doing any add calls, then do them all at once
+        #   use cursorholdi instead of textchangedi
+        #   still use textchangedi, but also use a timer, and if the highlight is less than X seconds old, don't recompute, just return
+        #   in insert mode, only recompute highlight groups on the line, or couple of lines surrounding the cursor
+        #   get the viewport of the current window, render that region only or first before the rest of the buffer
         mylexer = pygments.lexers.PythonLexer()
         buf     = self.nvim.buffers[0] # TODO can't hardcode this
         # TODO - can I be more intelligent than doing the whole buffer every time? just the area around a change?
         fullbuf = [line for line in buf] # TODO is this necessary / efficient?
 
-        # TODO need to subscribe to appropriate events (TextChanged[I]?) to run this function as needed
+        #  buf.clear_highlight(src_id=1, line_start=0, line_end=-1, async=True)
+        addid = 1 if self.srcset else 2
+        rmid  = 2 if self.srcset else 1
+        self.srcset = not self.srcset
         for linenum, line in enumerate(fullbuf, start=0):
             for (index, tokentype, value) in mylexer.get_tokens_unprocessed(line):
                 cols = len(value)
@@ -54,18 +76,21 @@ class Neosyntax(object):
                 # for a specific language if the generic way just won't work in all edge cases
                 # This should be possible both within this python code, and from vimscript
 
-                # another TODO is to figure out what exactly a src_id is, and whether I need to use it
+                # and another TODO is to map pygments tokens to vim highlight groups, so i don't have to write these all out
                 if tokentype == pygments.token.Name.Builtin:
-                    buf.add_highlight("Function", line=linenum, col_start=index, col_end=index+cols, src_id=1, async=True)
+                    buf.add_highlight("Function", line=linenum, col_start=index, col_end=index+cols, src_id=addid, async=True)
                 if tokentype == pygments.token.Name.Builtin.Pseudo:
-                    buf.add_highlight("Boolean", line=linenum, col_start=index, col_end=index+cols, src_id=1, async=True)
+                    buf.add_highlight("Boolean", line=linenum, col_start=index, col_end=index+cols, src_id=addid, async=True)
                 elif tokentype == pygments.token.Comment.Single or tokentype == pygments.token.Comment.Hashbang:
-                    buf.add_highlight("Comment", line=linenum, col_start=index, col_end=index+cols, src_id=1, async=True)
+                    buf.add_highlight("Comment", line=linenum, col_start=index, col_end=index+cols, src_id=addid, async=True)
                 elif tokentype == pygments.token.Keyword.Namespace:
-                    buf.add_highlight("Include", line=linenum, col_start=index, col_end=index+cols, src_id=4, async=True)
+                    buf.add_highlight("Include", line=linenum, col_start=index, col_end=index+cols, src_id=addid, async=True)
                 elif tokentype == pygments.token.Literal.Number.Integer:
-                    buf.add_highlight("Number", line=linenum, col_start=index, col_end=index+cols, src_id=5, async=True)
+                    buf.add_highlight("Number", line=linenum, col_start=index, col_end=index+cols, src_id=addid, async=True)
                 elif tokentype == pygments.token.Literal.String.Single or tokentype == pygments.token.Literal.String.Double:
-                    buf.add_highlight("String", line=linenum, col_start=index, col_end=index+cols, src_id=6, async=True)
+                    buf.add_highlight("String", line=linenum, col_start=index, col_end=index+cols, src_id=addid, async=True)
                 elif tokentype == pygments.token.Keyword or tokentype == pygments.token.Operator.Word:
-                    buf.add_highlight("Conditional", line=linenum, col_start=index, col_end=index+cols, src_id=7, async=True)
+                    buf.add_highlight("Conditional", line=linenum, col_start=index, col_end=index+cols, src_id=addid, async=True)
+
+        buf.clear_highlight(src_id=rmid, line_start=0, line_end=len(fullbuf), async=True)
+
